d47b637f438dd3e3d83048c4ae4145ddbab7fd7f
Diogo de Lorenzi Pinheiro <guitoempireallias@gmail.com>
2026-09-17 16:31:06 -0300

    chore: atualizar cypress para a versao 16

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
   e2e: {
     baseUrl: "http://127.0.0.1:4173",
     specPattern: "cypress/e2e/**/*.cy.js",
